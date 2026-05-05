d6f1fea60de68465fa9f673ca8d290a9e2a6e1a0
lytKateryna <lyt.kateryna@gmail.com>
2026-05-05 22:14:32 +0200

    Registered models in Django admin

--- rent_ads/admin.py
+++ rent_ads/admin.py
@@ -2,8 +2,39 @@ from django.contrib import admin
 from rent_ads.models import Listing, Booking, Review, SearchHistory, ListingView
 
 # Register your models here.
-admin.site.register(Listing)
 admin.site.register(Booking)
 admin.site.register(Review)
 admin.site.register(SearchHistory)
 admin.site.register(ListingView)
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'title',
+        'owner',
+        'price',
+        'city',
+        'is_active',
+        'created_at'
+    )
+
+    list_filter = (
+        'city',
+        'property_type',
+        'is_active',
+        'created_at'
+    )
+
+    search_fields = (
+        'title',
+        'description',
+        'address',
+        'city',
+        'district',
+        'owner__username'
+    )
+
+    ordering = ['-created_at']
+
+    list_per_page = 10
