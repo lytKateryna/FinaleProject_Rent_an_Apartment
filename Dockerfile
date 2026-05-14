7033b856df9ac1b952c4fdbf11faa81ba2b6e7ee
lytKateryna <lyt.kateryna@gmail.com>
2026-05-14 22:32:58 +0200

    Add Docker configuration and fix queryset filtering for bookings and reviews

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM python:3.13-slim AS builder
+
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1 \
+    PIP_DISABLE_PIP_VERSION_CHECK=1 \
+    PIP_NO_CACHE_DIR=1
+
+WORKDIR /build
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    build-essential \
+    default-libmysqlclient-dev \
+    pkg-config \
+    && rm -rf /var/lib/apt/lists/*
+
+
+COPY requirements.txt .
+
+RUN python -m venv /opt/venv && \
+    /opt/venv/bin/pip install --upgrade pip && \
+    /opt/venv/bin/pip install -r requirements.txt
+
+
+FROM python:3.13-slim AS runtime
+
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1 \
+    PATH="/opt/venv/bin:$PATH"
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        libmariadb3 \
+    && rm -rf /var/lib/apt/lists/* \
+
+WORKDIR /app
+
+
+RUN groupadd --system --gid 10001 app && \
+    useradd --system --uid 10001 --gid app --home-dir /app --shell /usr/sbin/nologin  app && \
+    mkdir -p /app/logs /app/avatars && \
+    chown -R app:app /app
+
+COPY --from=builder /opt/venv /opt/venv
+COPY --chown=app:app manage.py ./
+COPY --chown=app:app rent_ads ./rent_ads
+COPY --chown=app:app renthub ./renthub
+
+USER app
+
+EXPOSE 8000
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- rent_ads/views/bookings.py
+++ rent_ads/views/bookings.py
@@ -1,33 +1,49 @@
-from rest_framework import viewsets
 from drf_spectacular.utils import extend_schema, OpenApiExample
 from rest_framework import viewsets
 
 from rent_ads.models import Booking
 from rent_ads.serializers.booking import BookingSerializer
 from rent_ads.permissions import IsBookingOwnerOrLandlord
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class BookingViewSet(viewsets.ModelViewSet):
-    queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsBookingOwnerOrLandlord]
+
+    def get_queryset(self):
+        user = self.request.user
+        print("BOOKINGS USER:", user.email, user.username, user.id, user.role, user.is_superuser)
+        if user.is_anonymous:
+            return Booking.objects.none()
+        if user.is_superuser:
+            return Booking.objects.all()
+
+        user_role = getattr(user, 'role', None)
+
+        if user_role == 'tenant':
+            return Booking.objects.filter(tenant=user)
+        if user_role == 'landlord':
+            return Booking.objects.filter(listing__owner=user)
+        return Booking.objects.none()
 
     def perform_create(self, serializer):
         serializer.save(tenant=self.request.user)
 
-        @extend_schema(
-            examples=[
-                OpenApiExample(
-                    'Booking example',
-                    value={
-                        "listing": 3,
-                        "start_date": "2026-06-01",
-                        "end_date": "2026-06-10"
-                    },
-                    request_only=True,
-                ),
-            ]
-        )
-        def create(self, request, *args, **kwargs):
-            return super().create(request, *args, **kwargs)
-
+    @extend_schema(
+        examples=[
+            OpenApiExample(
+                'Booking example',
+                value={
+                    "listing": 3,
+                    "start_date": "2026-06-01",
+                    "end_date": "2026-06-10"
+                },
+                request_only=True,
+            ),
+        ]
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
